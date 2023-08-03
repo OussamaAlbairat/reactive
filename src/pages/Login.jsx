b38aa582319c4b6722735e107ce5c74f0ebbfcb3
OussamaAlbairat <oussama.albairat@gmail.com>
2023-08-02 21:07:43 -0400

    modified login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,95 @@
+import { useEffect, useState } from "react"
+import { postApiData } from "../store/Utils"
+
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const onChangeHandler = (e) => {
+    e.preventDefault()
+    if (e.target.name == "email") setEmail(e.target.value)
+    if (e.target.name == "password") setPassword(e.target.value)
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    const run = async () =>
+      postApiData("reactiveConfig/login", {
+        type: "login",
+        data: { email, password },
+      })
+    run()
+  }
+
   return (
-    <div
-      className="container d-flex justify-content-center"
-      style={{ height: "600px" }}
-    >
-      <div className="card my-auto">
-        <div className="card-header">Login with provider:</div>
-        <ul className="list-group list-group-flush">
-          {[
-            { provider: "aad", icon: "microsoft", label: "Microsoft Azure" },
-            { provider: "google", icon: "google", label: "Google" },
-            { provider: "github", icon: "github", label: "Github" },
-            { provider: "twitter", icon: "twitter", label: "Twitter" },
-          ].map((item, ndx) => {
-            return (
-              <li key={ndx} className="list-group-item">
-                <a
-                  style={{ width: "256px" }}
-                  href={`.auth/login/${item.provider}`}
-                  className={`btn btn-outline-secondary btn-lg btn-block my-2 bi bi-${item.icon}`}
-                >
-                  <span className="mx-2">{item.label}</span>
-                </a>
+    <div className="container-fluid row my-auto" style={{ height: "400px" }}>
+      <div className="col-4 my-auto mx-auto">
+        <form className="form">
+          <div className="card">
+            <div className="card-header text-center">Login</div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  id="email"
+                  name="email"
+                  className="form-control"
+                  type="email"
+                  placeholder="email"
+                  value={email}
+                  onChange={onChangeHandler}
+                />
               </li>
-            )
-          })}
-        </ul>
+              <li className="list-group-item form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  id="password"
+                  name="password"
+                  className="form-control"
+                  type="password"
+                  value={password}
+                  onChange={onChangeHandler}
+                />
+              </li>
+            </ul>
+            <button className="btn btn-success m-2" onClick={submitHandler}>
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="col-1 my-auto mx-auto text-center">
+        <u>Or</u>
+      </div>
+      <div className="col-5 my-auto mx-auto">
+        <div className="card">
+          <div className="card-header text-center">
+            Use an identity provider
+          </div>
+          <ul className="list-group list-group-flush">
+            {[
+              { provider: "aad", icon: "microsoft", label: "Microsoft Azure" },
+              { provider: "google", icon: "google", label: "Google" },
+              { provider: "github", icon: "github", label: "Github" },
+              { provider: "twitter", icon: "twitter", label: "Twitter" },
+            ].map((item, ndx) => {
+              return (
+                <li key={ndx} className="list-group-item">
+                  <div className="row">
+                    <a
+                      href={`.auth/login/${item.provider}`}
+                      className={`col-6 offset-3 btn btn-outline-secondary btn-lg btn-block my-2 bi bi-${item.icon}`}
+                    >
+                      <span className="d-none d-lg-inline mx-2">
+                        {item.label}
+                      </span>
+                    </a>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   )
--- src/store/Utils.jsx
+++ src/store/Utils.jsx
@@ -12,3 +12,28 @@ export const formatDate = (date) => {
 export const isInteger = (a) => {
   return !Number.isNaN(Number.parseInt(a))
 }
+
+export const getApiData = async (url) => {
+  const resp = await fetch(url)
+  if (!resp.ok) throw new Error("response not OK")
+  const data = await resp.json()
+  return data
+}
+
+export const postApiData = async (url, data = {}) => {
+  // Default options are marked with *
+  const resp = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "include", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  })
+  return resp.json() // parses JSON response into native JavaScript objects
+}
