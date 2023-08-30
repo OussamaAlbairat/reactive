78954ec5a9c07e6dcaad0ce1b3a2f587a4625ed3
OussamaAlbairat <oussama.albairat@gmail.com>
2023-08-30 11:58:39 -0400

    changed formatDate function

--- src/store/Utils.jsx
+++ src/store/Utils.jsx
@@ -4,9 +4,7 @@ export const financial = (x) => Number.parseFloat(x).toFixed(2)
 
 export const formatDate = (date) => {
   const dt = new Date(date)
-  return `${dt.getFullYear()}-${(dt.getMonth() + 1)
-    .toString()
-    .padStart(2, "0")}-${(dt.getDate() + 1).toString().padStart(2, "0")}`
+  return dt.toJSON().split("T")[0]
 }
 
 export const isInteger = (a) => {
