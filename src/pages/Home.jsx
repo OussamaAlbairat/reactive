4476978fb77f39eb88777e764acf039fb99fcc39
OussamaAlbairat <oussama.albairat@gmail.com>
2023-09-23 13:27:46 -0400

    trying colors and backgrounds

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-4 offset-4">
+        <div className="col-6 offset-2 py-5">
           <h1>Direct Portfolio Management</h1>
           <p>
             Is about exposing advanced financial tools and functions to the
