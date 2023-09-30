a2093d49b35e04d3943dc61476e1871fc4904655
OussamaAlbairat <oussama.albairat@gmail.com>
2023-09-30 11:19:30 -0400

    reorganize backtest form

--- src/pages/Backtest.jsx
+++ src/pages/Backtest.jsx
@@ -67,87 +67,91 @@ function Backtest() {
   if (strToBool(data[0].log_returns)) attrs.checked = true
 
   return (
-    <>
-      <div className="container row justify-content-center">
-        <div className="col-6">
-          <h4>Portfolio backtest</h4>
-          <form>
-            <div className="form-group my-1">
-              <label htmlFor="created">Created</label>
-              <input
-                id="created"
-                name="created"
-                type="date"
-                className="form-control"
-                onChange={inputChanged}
-                value={formatDate(data[0].created)}
-              />
-            </div>
-            <div className="form-group my-1">
-              <label htmlFor="description">Description</label>
-              <textarea
-                id="description"
-                name="description"
-                className="form-control"
-                onChange={inputChanged}
-                value={data[0].description}
-              />
-            </div>
-            <div className="form-group my-1">
-              <label htmlFor="portfolio_id">Portfolio</label>
-              <PortfoliosList
-                selectId="portfolio_id"
-                portfolioId={data[0].portfolio_id}
-                setPortfolioId={setPortfolioId}
-              />
-            </div>
-            <div className="form-group my-1">
-              <label htmlFor="start_date">Start Date</label>
-              <input
-                id="start_date"
-                name="start_date"
-                type="date"
-                className="form-control"
-                onChange={inputChanged}
-                value={formatDate(data[0].start_date)}
-              />
-            </div>
-            <div className="form-group my-1">
-              <label htmlFor="end_date">End Date</label>
-              <input
-                id="end_date"
-                name="end_date"
-                type="date"
-                className="form-control"
-                onChange={inputChanged}
-                value={formatDate(data[0].end_date)}
-              />
-            </div>
-            <div className="form-check my-1">
-              <label htmlFor="log_returns" className="form-check-label">
-                Log returns
-              </label>
-              <input {...attrs} />
-            </div>
-            <hr />
-            <div className="d-flex justify-content-end my-5">
-              <button
-                type="submit"
-                className="btn btn-danger"
-                onClick={saveClicked}
-              >
-                Save
-              </button>
-            </div>
-          </form>
+    <form className="container-fluid">
+      <div className="row">
+        <h4>Portfolio backtest</h4>
+      </div>
+      <div className="row">
+        <div className="col-4">
+          <div className="form-group my-1">
+            <label htmlFor="created">Created</label>
+            <input
+              id="created"
+              name="created"
+              type="date"
+              className="form-control"
+              onChange={inputChanged}
+              value={formatDate(data[0].created)}
+            />
+          </div>
+          <div className="form-group my-1">
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              name="description"
+              className="form-control"
+              onChange={inputChanged}
+              value={data[0].description}
+            />
+          </div>
+          <div className="form-group my-1">
+            <label htmlFor="portfolio_id">Portfolio</label>
+            <PortfoliosList
+              selectId="portfolio_id"
+              portfolioId={data[0].portfolio_id}
+              setPortfolioId={setPortfolioId}
+            />
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="form-group my-1">
+            <label htmlFor="start_date">Start Date</label>
+            <input
+              id="start_date"
+              name="start_date"
+              type="date"
+              className="form-control"
+              onChange={inputChanged}
+              value={formatDate(data[0].start_date)}
+            />
+          </div>
+          <div className="form-group my-1">
+            <label htmlFor="end_date">End Date</label>
+            <input
+              id="end_date"
+              name="end_date"
+              type="date"
+              className="form-control"
+              onChange={inputChanged}
+              value={formatDate(data[0].end_date)}
+            />
+          </div>
+          <div className="form-check my-1">
+            <label htmlFor="log_returns" className="form-check-label">
+              Log returns
+            </label>
+            <input {...attrs} />
+          </div>
         </div>
       </div>
-      <div className="container row justify-content-center">
+      <div className="row">
+        <hr />
+        <div className="d-flex justify-content-end my-5">
+          <button
+            type="submit"
+            className="btn btn-danger"
+            onClick={saveClicked}
+          >
+            Save
+          </button>
+        </div>
+      </div>
+      <div className="row">
         {data && data.length && data[0].report && (
           <Graph type="Portfolio" data={JSON.parse(data[0].report).data[0]} />
         )}
       </div>
-    </>
+    </form>
   )
 }
 export default Backtest
