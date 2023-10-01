95565f71132735b14c996e165fa019c77f0e3aa4
OussamaAlbairat <oussama.albairat@gmail.com>
2023-09-30 20:31:45 -0400

    added collapsable element to backtest form

--- src/pages/Backtest.jsx
+++ src/pages/Backtest.jsx
@@ -6,36 +6,31 @@ import { useSaving } from "../store/Saving"
 import PortfoliosList from "../components/PortfoliosList"
 import Graph from "../components/Graph"
 
-function toggleDisplay(id) {
-  let x = document.getElementById(id)
-  if (!x) return false
-  if (x.style.display === "none") x.style.display = "block"
-  else x.style.display = "none"
-  return true
-}
-
-function toggleBody(id_prefix) {
-  toggleDisplay(id_prefix + "-caret-bottom")
-  toggleDisplay(id_prefix + "-caret-right")
-  toggleDisplay(id_prefix + "-body")
-  return true
-}
-
 const Card = ({ id, title, children }) => {
+  const toggleDisplay = (id) => {
+    let x = document.getElementById(id)
+    if (!x) return false
+    if (x.style.display === "none") x.style.display = "block"
+    else x.style.display = "none"
+    return true
+  }
+
+  const toggleBody = (id_prefix) => {
+    toggleDisplay(id_prefix + "-caret-bottom")
+    toggleDisplay(id_prefix + "-caret-right")
+    toggleDisplay(id_prefix + "-body")
+    return true
+  }
+
   const headerClick = (e) => {
     e.preventDefault()
     toggleBody("graph")
   }
+
   return (
     <div id={id} className="card">
       <div id={`${id}-header`} className="card-header" onClick={headerClick}>
-        <div
-          style={{ display: "inline-block" }}
-          data-bs-toggle="collapse"
-          data-bs-target={`${id}-body`}
-          aria-expanded="false"
-          aria-controls={`${id}-body`}
-        >
+        <div className="d-inline-block">
           <svg
             id={`${id}-caret-bottom`}
             style={{ display: "block" }}
@@ -65,7 +60,7 @@ const Card = ({ id, title, children }) => {
             <path d="M5 15 L13 8 5 1 Z" />
           </svg>
         </div>
-        <h2 style={{ display: "inline-block" }}>{title}</h2>
+        <h4 className="mx-1 d-inline-block">{title}</h4>
       </div>
       <div id={`${id}-body`} className="card-body">
         {Children.map(children, (child, ndx) => {
@@ -76,57 +71,13 @@ const Card = ({ id, title, children }) => {
   )
 }
 
-function Backtest() {
-  const { id } = useParams()
-
-  const { data, setData } = useLoading({
-    url: isInteger(id) ? `/api/portfoliosmanagement/backtest?id=${id}` : null,
-    initData: [
-      {
-        id: id || -1,
-        created: "",
-        description: "",
-        start_date: "",
-        end_date: "",
-        log_returns: false,
-        report: "",
-      },
-    ],
-    cachedUrl: false,
-  })
-
-  const { save } = useSaving({ url: "/api/portfoliosmanagement/backtest" })
-
-  const inputChanged = (e) => {
-    e.preventDefault()
-    const name = e.target.name
-    const value = e.target.value
-    setData((old) => {
-      return [{ ...old[0], [name]: value }]
-    })
-  }
-
-  const logReturnsChanged = (e) => {
-    const name = e.target.name
-    const value = e.target.checked
-    setData((old) => {
-      return [{ ...old[0], [name]: value }]
-    })
-  }
-
-  const setPortfolioId = (value) => {
-    setData((old) => {
-      return [{ ...old[0], portfolio_id: value }]
-    })
-  }
-
-  const saveClicked = (e) => {
-    e.preventDefault()
-    save(data[0]).then((dt) => {
-      if (dt.status == "OK" && dt.data && dt.data.length) setData(dt.data)
-    })
-  }
-
+const Form = ({
+  data,
+  inputChanged,
+  setPortfolioId,
+  logReturnsChanged,
+  saveClicked,
+}) => {
   const attrs = {
     id: "log_returns",
     name: "log_returns",
@@ -138,11 +89,8 @@ function Backtest() {
   if (strToBool(data[0].log_returns)) attrs.checked = true
 
   return (
-    <form className="container">
-      <div className="row">
-        <h4>Portfolio backtest</h4>
-      </div>
-      <div className="row">
+    <div className="row">
+      <form>
         <div className="col-4">
           <div className="form-group my-1">
             <label htmlFor="created">Created</label>
@@ -213,6 +161,77 @@ function Backtest() {
             </button>
           </div>
         </div>
+      </form>
+    </div>
+  )
+}
+
+function Backtest() {
+  const { id } = useParams()
+
+  const { data, setData } = useLoading({
+    url: isInteger(id) ? `/api/portfoliosmanagement/backtest?id=${id}` : null,
+    initData: [
+      {
+        id: id || -1,
+        created: "",
+        description: "",
+        start_date: "",
+        end_date: "",
+        log_returns: false,
+        report: "",
+      },
+    ],
+    cachedUrl: false,
+  })
+
+  const { save } = useSaving({ url: "/api/portfoliosmanagement/backtest" })
+
+  const inputChanged = (e) => {
+    e.preventDefault()
+    const name = e.target.name
+    const value = e.target.value
+    setData((old) => {
+      return [{ ...old[0], [name]: value }]
+    })
+  }
+
+  const logReturnsChanged = (e) => {
+    const name = e.target.name
+    const value = e.target.checked
+    setData((old) => {
+      return [{ ...old[0], [name]: value }]
+    })
+  }
+
+  const setPortfolioId = (value) => {
+    setData((old) => {
+      return [{ ...old[0], portfolio_id: value }]
+    })
+  }
+
+  const saveClicked = (e) => {
+    e.preventDefault()
+    save(data[0]).then((dt) => {
+      if (dt.status == "OK" && dt.data && dt.data.length) setData(dt.data)
+    })
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <h4>Portfolio backtest</h4>
+      </div>
+      <div className="row">
+        <Card id="form" title="Form">
+          <Form
+            data={data}
+            inputChanged={inputChanged}
+            setPortfolioId={setPortfolioId}
+            logReturnsChanged={logReturnsChanged}
+            saveClicked={saveClicked}
+          />
+        </Card>
       </div>
       <div className="row">
         <hr />
@@ -224,7 +243,7 @@ function Backtest() {
           </Card>
         )}
       </div>
-    </form>
+    </div>
   )
 }
 export default Backtest
