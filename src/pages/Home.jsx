f2fe0ddb2cf79733653524bb5eca281fe056adff
OussamaAlbairat <oussama.albairat@gmail.com>
2023-10-20 08:15:27 -0400

    new portfolio image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,11 @@ const Home = () => {
       <div className="row">
         <div className="col-6 offset-2 py-5">
           <h1>Direct Portfolio Management</h1>
+          <img
+            className="rounded img-fluid rctv-img-lg"
+            src="/portfolio_growth.jpg"
+            alt="portfolio"
+          />
         </div>
       </div>
     </div>
