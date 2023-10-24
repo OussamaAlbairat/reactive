7cecd5e312dcd7f0370d7801d3cdea9cab5ae75b
OussamaAlbairat <oussama.albairat@gmail.com>
2023-10-24 18:24:37 -0400

    Azure AD B2C

--- src/authConfig.js
+++ src/authConfig.js
@@ -43,8 +43,10 @@ export const msalConfig = {
     clientId: "991b4086-210d-4967-9b84-1a1405329e8b", // This is the ONLY mandatory field that you need to supply.
     authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose SUSI as your default authority.
     knownAuthorities: [b2cPolicies.authorityDomain], // Mark your B2C tenant's domain as trusted.
-    redirectUri: "/", // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
-    postLogoutRedirectUri: "/", // Indicates the page to navigate after logout.
+    redirectUri:
+      "https://directportfoliomanagement.com/.auth/login/aadb2c/callback/", // You must register this URI on Azure Portal/App Registration. Defaults to window.location.origin
+    postLogoutRedirectUri:
+      "https://directportfoliomanagement.com/.auth/login/aadb2c/callback/", // Indicates the page to navigate after logout.
     navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
   },
   cache: {
